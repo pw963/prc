0e73a6ef8181855a8139d68872b1b8b50a60cacf
pw963 <78516518+pw963@users.noreply.github.com>
2021-08-24 18:57:42 -0400

    Update globals.py

--- config/globals.py
+++ config/globals.py
@@ -1,7 +1,9 @@
 extensions = [
-    "cogs.settings.setchannel", # Set log channel
-    "cogs.game_punishments.gban",# Game Ban
-    "cogs.game_punishments.gkick", # Game Kick
-    "cogs.game_punishments.gunban", # Game Unban
-    "cogs.game_punishments.gwarn" # Game Warn
-] # A list of all the extensions that will be loaded on bot startup.
+    "cogs.settings.setchannel",
+    "cogs.game_punishments.ban",
+    "cogs.game_punishments.kick",
+    "cogs.game_punishments.unban",
+    "cogs.game_punishments.warn",
+    "cogs.verification.verify",
+    "cogs.help"
+]
