3b68ce5b1af9d493564379ff6a203962283a5c6f
pw963 <78516518+pw963@users.noreply.github.com>
2021-08-24 18:52:56 -0400

    Update setchannel.py

--- cogs/settings/setchannel.py
+++ cogs/settings/setchannel.py
@@ -1,60 +1,60 @@
-# Importing
 import discord
 from discord import Embed
 from discord.ext import commands
-from db.mongodb import settings # Loading the 'settings' collection.
+from db.mongodb import settings
 
 class Settings(commands.Cog):
     def __init__(self, client):
-        self.client = client # Initialization
-    # The 'set-channel' command. Sets a settings for a channel, which will make the channel log punishments. The event the channel will log depends on the channel type that is specified (1st argument).
-    # The channel that will receive the settings is the channel that is pinged (2nd argument)
+        self.client = client
+
     @commands.command(
         name="set-channel",
-        aliases=["setchannel", "channel"]
+        aliases=["setchannel", "channel"],
+        usage = "`g.set-channel warns #warnings`",
+        help = "Settings ##Sets the channel specified to log punishments for the specified type (warns | kicks | bans). Once set, it may be updated by running the command again. ##`Punishment Type`, `Channel` ##`Manage Channels`"
     )
     @commands.has_permissions(manage_channels=True)
     async def set_channel(self, ctx, chType=None, channel: discord.TextChannel=None):
-        if chType == None or channel == None: # If the channel type or the channel is not specified.
+        if chType == None or channel == None:
 
-            em = Embed(
+            noData = Embed(
                 description = "",
                 color = discord.Color.blue()
             )
 
             arr = []
             for item in settings.find({"guild":ctx.guild.id}):
-                arr.append(item) # Looking for how many settings the guild has in the collection, then adding their data to 'arr'.
+                arr.append(item)
             
-            if len(arr) == 0: # If the guild has no settings set.
-                em.description += f"Nothing yet."
-                return await ctx.send("Correct usage: `.set-channel [warns | kicks | bans] [#channel]`", embed=em)
-            else: # If there are settings set for the guild.
-                for setting in settings.find({"guild":ctx.guild.id}): # Looping through the settings that the guild has.
-                    em.description += f"**{setting['type']} Channel** `-` <#{setting['_id']}>\n" # Adding the setting's type, and the setting's channel.
+            if len(arr) == 0:
+                noData.description += f"Nothing yet."
+                return await ctx.send("Correct usage: `g.set-channel [warns | kicks | bans] [#channel]`", embed=noData)
+            else:
+                for setting in settings.find({"guild":ctx.guild.id}):
+                    noData.description += f"**{setting['type']} Channel** `-` <#{setting['_id']}>\n"
                 
-                return await ctx.send("Correct usage: `.set-channel [warns | kicks | bans] [#channel]`", embed=em)
+                return await ctx.send("Correct usage: `g.set-channel [warns | kicks | bans] [#channel]`", embed=noData)
         else:
-            if chType.lower() == "warns": # If the channel type specified is 'warns'
-                if settings.find_one({"type":"Warns", "guild":ctx.guild.id}): settings.delete_one({"type":"Warns", "guild":ctx.guild.id}) # Checking if a setting for warns exists already. If it does, the original setting will be deleted
-                settings.insert_one({"_id":channel.id, "type":"Warns", "guild":ctx.guild.id}) # Creating the new setting
+            if chType.lower() == "warns":
+                if settings.find_one({"type":"Warns", "guild":ctx.guild.id}): settings.delete_one({"type":"Warns", "guild":ctx.guild.id})
+                settings.insert_one({"_id":channel.id, "type":"Warns", "guild":ctx.guild.id})
 
                 return await ctx.send(f"Successfully set `#{channel.name}` as the warnings channel. All warns will now be logged there.")
             
-            elif chType.lower() == "kicks": # If the channel type specified is 'kicks'
-                if settings.find_one({"type":"Kicks", "guild":ctx.guild.id}): settings.delete_one({"type":"Kicks", "guild":ctx.guild.id}) # Checking if a setting for kicks exists already. If it does, the original setting will be deleted
-                settings.insert_one({"_id":channel.id, "type":"Kicks", "guild":ctx.guild.id}) # Creating new setting
+            elif chType.lower() == "kicks":
+                if settings.find_one({"type":"Kicks", "guild":ctx.guild.id}): settings.delete_one({"type":"Kicks", "guild":ctx.guild.id})
+                settings.insert_one({"_id":channel.id, "type":"Kicks", "guild":ctx.guild.id})
 
                 return await ctx.send(f"Successfully set `#{channel.name}` as the kicks channel. All kicks will now be logged there.")
             
-            elif chType.lower() == "bans": # If the channel type specified is 'bans'
-                if settings.find_one({"type":"Bans", "guild":ctx.guild.id}): settings.delete_one({"type":"Bans", "guild":ctx.guild.id}) # Checking if a setting for bans exists already. If it does, the original setting will be deleted
-                settings.insert_one({"_id":channel.id, "type":"Bans", "guild":ctx.guild.id}) # Creating new setting
+            elif chType.lower() == "bans":
+                if settings.find_one({"type":"Bans", "guild":ctx.guild.id}): settings.delete_one({"type":"Bans", "guild":ctx.guild.id})
+                settings.insert_one({"_id":channel.id, "type":"Bans", "guild":ctx.guild.id})
 
                 return await ctx.send(f"Successfully set `#{channel.name}` as the bans channel. All bans will now be logged there.")
             
-            else: # If the channel type specified is not 'kicks', 'bans', or 'warns'.
+            else:
                 return await ctx.send(f"Unknown channel type. Please choose warns, kicks, or bans.")
-# Adding the cog
+
 def setup(client):
     client.add_cog(Settings(client))
